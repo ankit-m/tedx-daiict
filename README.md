39772b808bec6384f2529b4b31168569a946db8a
ankit-m <muchhalaankit@gmail.com>
2015-10-31 15:50:11 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# tedx-daiict
---
+++ js/init.js
@@ -0,0 +1,13 @@
+(function($) {
+    $(function() {
+
+        $('.button-collapse').sideNav();
+
+        $(document)
+            .ready(function() {
+                $('.slider').slider({
+                    full_width: true
+                });
+            });
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
