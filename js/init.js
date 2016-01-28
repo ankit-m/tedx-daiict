d0748aaaa11b0c3aa6b253774cc81484627fa6be
Ankit Muchhala <muchhalaankit@gmail.com>
2016-01-29 01:36:50 +0530

    dialog box add

--- js/init.js
+++ js/init.js
@@ -9,5 +9,12 @@
                     full_width: true
                 });
             });
+
+        $('.modal-trigger').leanModal({
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .5, // Opacity of modal background
+            in_duration: 300, // Transition in duration
+            out_duration: 200 // Transition out duration
+        });
     }); // end of document ready
 })(jQuery); // end of jQuery name space
